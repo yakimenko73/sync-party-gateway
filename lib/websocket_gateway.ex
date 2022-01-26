de5933d57b22a22445f698b6048717a369e7cd4c
Roman <53937692+yakimenko73@users.noreply.github.com>
2022-01-26 18:08:43 +0200

    add websocket handler

--- lib/websocket_gateway.ex
+++ lib/websocket_gateway.ex
@@ -1,18 +1,32 @@
 defmodule WebsocketGateway do
-  @moduledoc """
-  Documentation for `WebsocketGateway`.
-  """
+  def start(_type, _args) do
+    children = [
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: WebsocketGateway.Router,
+        options: [
+          dispatch: dispatch(),
+          port: 4000
+        ]
+      ),
+      Registry.child_spec(
+        keys: :duplicate,
+        name: Registry.WebsocketGateway
+      )
+    ]
 
-  @doc """
-  Hello world.
+    opts = [strategy: :one_for_one, name: WebsocketGateway.Application]
+    Supervisor.start_link(children, opts)
+  end
 
-  ## Examples
-
-      iex> WebsocketGateway.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp dispatch do
+    [
+      {:_,
+        [
+          {"/ws/[...]", WebsocketGateway.SocketHandler, []},
+          {:_, Plug.Cowboy.Handler, {WebsocketGateway.Router, []}}
+        ]
+      }
+    ]
   end
 end
