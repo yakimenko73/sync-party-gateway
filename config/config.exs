c3d396209f49ee6cb01e3b49df9a5a620c7259e6
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-06 15:46:31 +0200

    config: add dev config file

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,3 @@
-use Mix.Config
+import Config
 
-config :websocket_gateway,
-       ip: {0, 0, 0, 0},
-       port: 4000,
-       timeout: 60000,
-       ws_endpoint: "ws",
-       db_name: "sync-party",
-       db_username: "admin",
-       db_password: "admin",
-       db_hostname: "localhost",
-       db_port: 27017
+import_config "#{config_env()}.exs"
