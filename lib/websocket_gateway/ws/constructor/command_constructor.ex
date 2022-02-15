f0842df770a2a8702c1729505dd96303a1f8db2e
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-15 14:51:00 +0200

    ws/constructor: add data key

--- lib/websocket_gateway/ws/constructor/command_constructor.ex
+++ lib/websocket_gateway/ws/constructor/command_constructor.ex
@@ -2,6 +2,6 @@ defmodule WebsocketGateway.Constructor.CommandConstructor do
   @behaviour WebsocketGateway.Constructor.Constructor
 
   def construct(text) do
-    %{message: %{text: text}}
+    %{data: %{message: %{text: text}}}
   end
 end
--- lib/websocket_gateway/ws/constructor/message_constructor.ex
+++ lib/websocket_gateway/ws/constructor/message_constructor.ex
@@ -2,14 +2,14 @@ defmodule WebsocketGateway.Constructor.MessageConstructor do
   @behaviour WebsocketGateway.Constructor.Constructor
 
   def construct(text) do
-    %{message: %{text: text}}
+    %{data: %{message: %{text: text}}}
   end
 
   def construct(text, %{nickname: nickname, color: color}) do
-    %{message: %{text: text, nickname: nickname, color: color}}
+    %{data: %{message: %{text: text, nickname: nickname, color: color}}}
   end
 
   def construct(text, %{nickname: nickname}) do
-    %{message: %{text: text, nickname: nickname}}
+    %{data: %{message: %{text: text, nickname: nickname}}}
   end
 end
