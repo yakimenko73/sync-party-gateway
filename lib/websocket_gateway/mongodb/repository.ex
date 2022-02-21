75959eb11ffc17f08f8469e2b8135f98ad39c573
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-21 13:27:33 +0200

    mongodb: add room member func

--- lib/websocket_gateway/mongodb/repository.ex
+++ lib/websocket_gateway/mongodb/repository.ex
@@ -1,9 +1,14 @@
 defmodule WebsocketGateway.MongoDb.Repository do
+  # may be carry ?
   def list(collection, body, limit, sort) do
     :mongo |> Mongo.find(collection, body, sort: sort, limit: limit)
   end
 
   def add(collection, body) do
     :mongo |> Mongo.insert_one(collection, body)
+  end
+
+  def update_one(collection, filter, body, upsert) do
+    :mongo |> Mongo.update_one(collection, filter, body, upsert: upsert)
   end
 end
--- lib/websocket_gateway/mongodb/service.ex
+++ lib/websocket_gateway/mongodb/service.ex
@@ -2,6 +2,7 @@ defmodule WebsocketGateway.MongoDb.Service do
   alias WebsocketGateway.MongoDb.Repository
 
   @collection_chat_messages "chatmessages"
+  @collection_room_members "roommembers"
 
   def get_last_messages(chat_id, limit) do
     messages =
@@ -22,6 +23,25 @@ defmodule WebsocketGateway.MongoDb.Service do
       color: user.color,
       date: DateTime.utc_now()
     })
+
+    :ok
+  end
+
+  def add_room_member(room_id, member) do
+    body = %{
+      room_id: room_id,
+      nickname: member.nickname,
+      color: member.color
+    }
+
+    @collection_room_members
+    |> Repository.update_one(
+      body,
+      %{
+        "$set" => Map.put(body, :date, DateTime.utc_now())
+      },
+      true
+    )
 
     :ok
   end
