4d0addbd69f4f3f55aa62adef9e7b27324578c0d
yakimenko73 <r.yakimenko.73@gmail.com>
2022-02-22 14:07:17 +0200

    utils: add bson util

--- lib/websocket_gateway/utils.ex
+++ lib/websocket_gateway/utils.ex
@@ -3,4 +3,14 @@ defmodule WebsocketGateway.Utils do
   def to_atom_keys(map) do
     for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
   end
+
+  def get_value_from_bson(bson) when is_list(bson) do
+    bson
+    |> List.first()
+    |> BSON.ObjectId.encode!()
+  end
+
+  def get_value_from_bson(bson) do
+    bson |> BSON.ObjectId.encode!()
+  end
 end
